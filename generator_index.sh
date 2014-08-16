fb5a4e4f47fce37a93f9b5c820517d28f02b8f12
leiming <leming@360.cn>
2014-08-16 18:18:45 +0800

    fix bug

--- generator_index.sh
+++ generator_index.sh
@@ -37,7 +37,7 @@ if [[ -r "$1"  ]]; then
         echo -e "--- generate pylon_tags ---\n"
         cd $pylon_src
         ctags -R --tag-relative  --fields=+aimS --languages=php -f $1/pylon_tags
-        cp $1/pylon_tags $1/_prj/pylon_tags
+        mv $1/pylon_tags $1/_prj/pylon_tags
         cd $1
         find $pylon_src/ -name '*.php'  >> $csfile
     fi
